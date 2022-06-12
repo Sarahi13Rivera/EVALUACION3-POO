971d53ec39b38763f5889f6849fe9e5a1d8fe827
Sarahi Rivera <99987448+Sarahi13Rivera@users.noreply.github.com>
2022-06-11 18:04:52 -0600

    Add files via upload

---
+++ EVA3_15_THROW_PERSONA/src/eva3_15_throw_persona/EVA3_15_THROW_PERSONA.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_15_throw_persona;
+
+/**
+ *
+ * @author HP1000
+ */
+public class EVA3_15_THROW_PERSONA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Persona persona = new Persona();
+        persona.setNombre("SARAHI RIVERA");
+        try {
+            persona.setEdad(20);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        try {
+            Persona persona2 = new Persona("ANDRES SAENZ", 20);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) throws Exception {
+        this.nombre = nombre;
+        setEdad(edad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) throws Exception {
+        if(edad >= 0 && edad <= 130){
+            this.edad = edad;
+        }else{
+            throw new Exception("Rango de edad incorrecto");
+        }
+    }
+}
+    
+
